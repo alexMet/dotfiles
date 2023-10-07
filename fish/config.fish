3630188439ef9ab549397a87421c9364083950e5
Alexandros Metaxas <metaxas.alexan@gmail.com>
2023-10-07 15:08:06 +0300

    Add fish config

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# locale
+set --export LANG en_US.UTF-8
+set --export LC_MESSAGES "C"
+
+# search
+set --export FZF_DEFAULT_COMMAND "rg --files --hidden --follow"
+set --export FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+
+# theme
+fish_config theme choose "Rosé Pine"

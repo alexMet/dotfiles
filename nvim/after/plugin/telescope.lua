3e82c01e8f8f499fe3bed64d4b490eb5e7591c25
Alexandros Metaxas <metaxas.alexan@gmail.com>
2024-01-15 17:47:11 +0200

    Add more telescope remaps

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,12 +1,21 @@
 local builtin = require('telescope.builtin')
 require('telescope').load_extension('fzf')
 
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+--- Git related search remaps
 vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
 vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
-vim.keymap.set('n', '<leader>gs', builtin.git_stash, {})
+vim.keymap.set('n', '<leader>gC', builtin.git_bcommits, {})
+vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
+vim.keymap.set('n', '<leader>gS', builtin.git_stash, {})
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fc', builtin.grep_string, {})
+--- Other search remaps
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fc', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>fC', builtin.commands, {})
+vim.keymap.set('n', '<leader>ff', function() builtin.find_files({ hidden = true, no_ignore = true }) end, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
+vim.keymap.set('n', '<leader>ft', function() builtin.colorscheme({ enable_preview = true }) end, {})
+vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})

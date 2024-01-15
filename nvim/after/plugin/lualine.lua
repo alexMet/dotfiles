412d8c10636383558d9ffd3a400e034d8127b16b
Alexandros Metaxas <metaxas.alexan@gmail.com>
2024-01-15 17:48:14 +0200

    Format and simplify lualine

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,15 +1,15 @@
-require('lualine').setup {
+require('lualine').setup({
   options = {
     icons_enabled = false,
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
+    component_separators = '',
+    section_separators = '',
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {'buffers'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  }
-}
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch' },
+    lualine_c = { 'buffers' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' },
+  },
+})

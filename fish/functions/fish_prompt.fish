b6bb2ca78ea79c19777315b00e1b7e413d2bd362
Alexandros Metaxas <metaxas.alexan@gmail.com>
2024-07-15 21:55:16 +0300

    Make fish prompt less minimal

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,6 +1,36 @@
-function fish_prompt
-    set_color $fish_color_cwd
-    echo -n (basename $PWD)
-    set_color normal
-    echo -n ' λ '
+function fish_prompt --description 'Write out the prompt'
+    set -l last_status $status
+    set -l normal (set_color normal)
+    set -l status_color (set_color -o brgreen)
+    set -l cwd_color (set_color $fish_color_cwd)
+    set -l vcs_color (set_color -o brpurple)
+    set -l prompt_status ""
+
+    # Since we display the prompt on a new line allow the directory names to be longer.
+    set -q fish_prompt_pwd_dir_length
+    or set -lx fish_prompt_pwd_dir_length 0
+
+    # Color the prompt differently when we're root
+    set -l suffix 'λ'
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set cwd_color (set_color $fish_color_cwd_root)
+        end
+        set suffix '#'
+    end
+
+    # Color the prompt in red on error
+    if test $last_status -ne 0
+        set status_color (set_color $fish_color_error)
+        set prompt_status $status_color "[" $last_status "]" $normal
+    end
+
+    # Color and set the virtual env if enabled
+    if test -n "$VIRTUAL_ENV"
+        set venv_color (set_color -o blue)
+        set venv_name $venv_color "(" (basename $VIRTUAL_ENV) ") " $normal
+    end
+
+    echo -s $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
+    echo -n -s $venv_name $status_color $suffix ' ' $normal
 end

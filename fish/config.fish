b85c46a2b727fb2aca27a5ec2bbdbb3f2de94dcb
Alexandros Metaxas <metaxas.alexan@gmail.com>
2024-12-24 00:57:01 +0200

    feat(fish): Setup fzf and add nvim alias

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,8 @@ set --export LANG en_US.UTF-8
 set --export LC_MESSAGES "C"
 
 # search
+fzf --fish | source
+set --export FZF_DEFAULT_OPTS "--height 40% --layout reverse --border"
 set --export FZF_DEFAULT_COMMAND "rg --files --hidden --follow"
 set --export FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 

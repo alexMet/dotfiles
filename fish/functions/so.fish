778cc7cf8fddf9417528a372d08966d801ff16f0
Alexandros Metaxas <metaxas.alexan@gmail.com>
2025-03-16 03:06:52 +0200

    feat: add fish functions for updating system deps and sourcing venvs

---
+++ fish/functions/so.fish
@@ -0,0 +1,3 @@
+function so --wraps='source .venv/bin/activate.fish' --description 'Activate the virtual environment'
+  source .venv/bin/activate.fish
+end
---
+++ fish/functions/upup.fish
@@ -0,0 +1,37 @@
+function upup --description 'Update system packages'
+    function update_if_needed
+        # Check if the local branch is behind the remote
+        git fetch origin master
+        set behind (git rev-list --count HEAD..origin/master)
+
+        if test $behind -gt 0
+            echo "==> Updates found: $behind commits behind."
+            git pull origin master
+            makepkg -sirc
+            rm *.zip *.zst*
+        else
+            echo "==> Already up to date."
+        end
+    end
+
+    echo "==> Updating brave..."
+    pushd .
+    cd ~/Documents/brave-bin/
+    update_if_needed
+    popd
+
+    echo "==> Updating openvpn3..."
+    pushd .
+    cd ~/Documents/openvpn3/
+    update_if_needed
+    popd
+
+    echo "==> Updating system packages..."
+    sudo pacman -Syu
+
+    # NOTE: https://wiki.archlinux.org/title/Pacman/Pacnew_and_Pacsave
+    # Check the diff manually and merge the changes with the old config
+    # diff /etc/pacman.conf /etc/pacman.conf.pacnew
+    echo "==> Checking for new config files..."
+    sudo find /etc -name '*.pacnew' -o -name '*.pacsave'
+end

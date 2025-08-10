c78180e194b8f9fdb51e6d0b8783df41c62b3f09
Alexandros Metaxas <metaxas.alexan@gmail.com>
2025-08-10 14:43:41 +0300

    feat(fish): remove brave when updating

--- fish/functions/clinex.fish
+++ fish/functions/clinex.fish
@@ -1,27 +1,32 @@
 function clinex --description 'Clean up system'
-	echo "==> Cleaning container stuff..."
-	podman system prune -f
+    echo "==> Cleaning container stuff..."
+    podman system prune -f
 
-	echo "==> Cleaning pacman cache..."
-	du -sh /var/cache/pacman/pkg/
-	sudo pacman -Scc
+    echo "==> Cleaning pacman cache..."
+    du -sh /var/cache/pacman/pkg/
+    sudo pacman -Scc
 
-	echo "==> Listing unused packages..."
-	pacman -Qtdq
+    echo "==> Cleaning home folder cache..."
+    du -sh ~/.cache/
+    rm -rf ~/.cache/*
 
-	echo "==> Cleaning home folder cache..."
-	du -sh ~/.cache/
-	rm -rf ~/.cache/*
-	
     echo "==> Cleaning old logs from journal..."
-	du -sh /var/log/journal/
-	sudo journalctl --vacuum-size=50M
+    du -sh /var/log/journal/
+    sudo journalctl --vacuum-size=50M
 
     # TODO: Find a way to do this safely
-	# echo "==> Cleaning old config files..."
-	# ls -lah ~/.local/share/
-	# ls -lah ~/.config/
+    # echo "==> Cleaning old config files..."
+    # ls -lah ~/.local/share/
+    # ls -lah ~/.config/
 
     # TODO: Find all git folders and clean them
-    # git branch --merged master | grep -v "master" | xargs git branch -d && git fetch --prune && git clean -f -d && git gc --prune=now --aggressive
+    # git branch --merged master
+    # | grep -v "master"
+    # | xargs git branch -d
+    # && git fetch --prune
+    # && git clean -f -d
+    # && git gc --prune=now --aggressive
+
+    echo "==> Listing unused packages..."
+    pacman -Qtdq
 end
--- fish/functions/upup.fish
+++ fish/functions/upup.fish
@@ -14,23 +14,17 @@ function upup --description 'Update system packages'
         end
     end
 
-    echo "==> Updating brave..."
-    pushd .
-    cd ~/Documents/brave-bin/
-    update_if_needed
-    popd
+    # NOTE: https://wiki.archlinux.org/title/Reflector
+    echo "==> Updating mirrorlist..."
+    sudo reflector --country Greece,France,Germany --latest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+    echo "==> Updating system packages..."
+    sudo pacman -Syu
 
     echo "==> Updating openvpn3..."
     pushd .
     cd ~/Documents/openvpn3/
     update_if_needed
     popd
-
-    # NOTE: https://wiki.archlinux.org/title/Reflector
-    echo "==> Updating mirrorlist..."
-    sudo reflector --country Greece,France,Germany --latest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-    echo "==> Updating system packages..."
-    sudo pacman -Syu
 
     # NOTE: https://wiki.archlinux.org/title/Pacman/Pacnew_and_Pacsave
     # Check the diff manually and merge the changes with the old config

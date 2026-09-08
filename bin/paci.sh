cc2cba8204f185ade358fcf7a876b512d73908a8
Alexandros Metaxas <metaxas.alexan@gmail.com>
2026-09-08 18:21:29 +0300

    chore: fix scripts bug with stdin and add iwd to packages

--- bin/paci.sh
+++ bin/paci.sh
@@ -3,15 +3,14 @@ set -euo pipefail
 
 # List available packages, with info shown in a preview window, and install selected ones
 packages=$(pacman -Slq | fzf --multi --preview 'pacman -Si {1}')
+mapfile -t selected <<< "$packages"
 
-if [[ -n "$packages" ]]; then
-    while IFS= read -r package; do
-        sudo pacman -S "$package"
-        if [[ $? -eq 0 ]]; then
-            description=$(pacman -Si "$package" | grep Description | cut -d ':' -f 2)
-            echo "$package --$description" >> "$MY_PACKAGES"
-        fi
-    done <<< "$packages"
-fi
+for package in "${selected[@]}"; do
+    sudo pacman -S "$package"
+    if [[ $? -eq 0 ]]; then
+        description=$(pacman -Si "$package" | grep Description | cut -d ':' -f 2)
+        echo "$package --$description" >> "$MY_PACKAGES"
+    fi
+done
 
 sort -u -o "$MY_PACKAGES" "$MY_PACKAGES"
--- bin/pacrm.sh
+++ bin/pacrm.sh
@@ -3,12 +3,11 @@ set -euo pipefail
 
 # List all your installed packages, and then remove selected packages
 packages=$(pacman -Qq | fzf --multi --preview 'pacman -Qi {1}')
+mapfile -t selected < <(pacman -Qq | fzf --multi --preview 'pacman -Qi {1}')
 
-if [[ -n "$packages" ]]; then
-    while IFS= read -r package; do
-        sudo pacman -Rns "$package"
-        if [[ $? -eq 0 ]]; then
-            sed -i "/$package --/d" "$MY_PACKAGES"
-        fi
-    done <<< "$packages"
-fi
+for package in "${selected[@]}"; do
+    sudo pacman -Rns "$package"
+    if [[ $? -eq 0 ]]; then
+        sed -i "/$package --/d" "$MY_PACKAGES"
+    fi
+done
